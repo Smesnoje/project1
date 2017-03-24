9318076a27fc13d8b6a58f1f09a20aaa43941aa7
anarthul <anarthul@gmail.com>
2017-03-24 11:45:53 +0100

    Ne menjaj sifru mamlaze

--- sites/all/modules/basic_cart/basic_cart_cart_render_block.tpl.php
+++ sites/all/modules/basic_cart/basic_cart_cart_render_block.tpl.php
@@ -6,20 +6,20 @@
 ?>
 
 <?php if (empty($cart)): ?>
-  <p><?php print t('Your cart is empty.'); ?></p>
+  <p><?php print t('Korpa je prazna.'); ?></p>
 <?php else: ?>
   <div class="basic-cart-grid basic-cart-block">
     <?php if(is_array($cart) && count($cart) >= 1): ?>
       <?php foreach($cart as $nid => $node): ?>
         <div class="basic-cart-cart-contents row">
           <div class="basic-cart-cart-node-title cell"><?php print l($node->title, 'node/' . $node->nid); ?></div>
-        
+
           <div class="basic-cart-cart-quantity cell"><?php print $node->basic_cart_quantity; ?></div>
           <div class="basic-cart-cart-x cell">x</div>
           <div class="basic-cart-cart-unit-price cell">
             <strong><?php print basic_cart_price_format($node->basic_cart_unit_price); ?></strong>
           </div>
-            
+
         </div>
       <?php endforeach; ?>
       <div class="basic-cart-cart-total-price-contents row">
